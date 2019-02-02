46048e6de6592814ab9edf02573fd5e727ab4749
Kevin Miller <kevin@csumb.edu>
2019-02-02 10:20:15 -0800

    Changed rowId to recordId

--- gatsby-node.js
+++ gatsby-node.js
@@ -98,7 +98,7 @@ exports.sourceNodes = async (
       id: createNodeId(`Airtable_${row.id}`),
       parent: null,
       table: row._table.name,
-      rowId: row.id,
+      recordId: row.id,
       queryName: row.queryName,
       children: [],
       internal: {
