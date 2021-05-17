d68c5fb0f39afc472af5de3a3eee662b4da7baf5
Jacob Bolda <me@jacobbolda.com>
2021-05-17 00:49:20 -0500

    chore: setup mocked examples on Github Actions

--- examples/recipes-with-multi-type/gatsby-config.js
+++ examples/recipes-with-multi-type/gatsby-config.js
@@ -1,26 +1,73 @@
+const nock = require("nock");
+const fs = require("fs");
+/* helper to create the fixture
+// uncomment this, run gatsby build, and it will create a record.txt
+// those are nock functions that can be copied below to mock out the API requests
+
+const appendLogToFile = (content) => {
+  return fs.appendFileSync("record.txt", content);
+};
+nock.recorder.rec({
+  logging: appendLogToFile,
+});
+*/
+
+nock("https://api.airtable.com:443", { encodedQueryParams: true })
+  .get("/v0/appM8D8wmSJX9WJDE/Recipes")
+  .query({ view: "List" })
+  .reply(
+    200,
+    (uri, requestBody, cb) => {
+      fs.readFile("__fixtures__/recipes.json", cb); // Error-first callback
+    },
+    [],
+  );
+
+nock("https://api.airtable.com:443", { encodedQueryParams: true })
+  .get("/v0/appM8D8wmSJX9WJDE/Style")
+  .query({ view: "Main%20View" })
+  .reply(
+    200,
+    (uri, requestBody, cb) => {
+      fs.readFile("__fixtures__/style.json", cb); // Error-first callback
+    },
+    [],
+  );
+
+nock("https://api.airtable.com:443", { encodedQueryParams: true })
+  .get("/v0/appM8D8wmSJX9WJDE/Cooking%20Method")
+  .query({ view: "Main%20View" })
+  .reply(
+    200,
+    (uri, requestBody, cb) => {
+      fs.readFile("__fixtures__/cooking_method.json", cb); // Error-first callback
+    },
+    [],
+  );
+
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Recipes"
+    title: "Gatsby Recipes",
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`
-      }
+        path: `${__dirname}/src/pages/`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
-        path: `${__dirname}/src/assets/`
-      }
+        path: `${__dirname}/src/assets/`,
+      },
     },
     {
       resolve: `gatsby-source-airtable`,
       options: {
-        apiKey: process.env.AIRTABLE_API_KEY_DEV, //(set via environment variable for this example)
+        apiKey: process.env.AIRTABLE_DEV_API_KEY, //(set via environment variable for this example)
         tables: [
           {
             baseId: `appM8D8wmSJX9WJDE`,
@@ -30,30 +77,30 @@ module.exports = {
             mapping: {
               Attachments: `fileNode`,
               Ingredients: "text/markdown",
-              Directions: "text/markdown"
+              Directions: "text/markdown",
             },
             tableLinks: [`Cooking_Method`, `Style`],
             separateNodeType: true,
-            separateMapType: true
+            separateMapType: true,
           },
           {
             baseId: `appM8D8wmSJX9WJDE`,
             tableName: `Cooking Method`,
             tableView: `Main View`,
-            tableLinks: [`Recipes`]
+            tableLinks: [`Recipes`],
           },
           {
             baseId: `appM8D8wmSJX9WJDE`,
             tableName: `Style`,
             tableView: `Main View`,
-            tableLinks: [`Recipes`]
-          }
-        ]
-      }
+            tableLinks: [`Recipes`],
+          },
+        ],
+      },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-mdx`,
-    `gatsby-plugin-react-helmet`
-  ]
+    `gatsby-plugin-react-helmet`,
+  ],
 };
--- examples/testing-options/gatsby-config.js
+++ examples/testing-options/gatsby-config.js
@@ -1,13 +1,49 @@
+const nock = require("nock");
+const fs = require("fs");
+/* helper to create the fixture
+// uncomment this, run gatsby build, and it will create a record.txt
+// those are nock functions that can be copied below to mock out the API requests
+
+const appendLogToFile = (content) => {
+  return fs.appendFileSync("record.txt", content);
+};
+nock.recorder.rec({
+  logging: appendLogToFile,
+});
+*/
+
+nock("https://api.airtable.com:443", { encodedQueryParams: true })
+  .get("/v0/appv8dCu3p2PAaA2F/Sections")
+  .query({ view: "All" })
+  .reply(
+    200,
+    (uri, requestBody, cb) => {
+      fs.readFile("__fixtures__/sections.json", cb); // Error-first callback
+    },
+    [],
+  );
+
+nock("https://api.airtable.com:443", { encodedQueryParams: true })
+  .get("/v0/appv8dCu3p2PAaA2F/Pages")
+  .query({ view: "All" })
+  .reply(
+    200,
+    (uri, requestBody, cb) => {
+      fs.readFile("__fixtures__/pages.json", cb); // Error-first callback
+    },
+    [],
+  );
+
 module.exports = {
   siteMetadata: {
-    title: 'My Documentation Site',
+    title: "My Documentation Site",
   },
   plugins: [
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-airtable`,
       options: {
-        apiKey: process.env.AIRTABLE_API_KEY, // (set via environment variable for this example)
+        apiKey: process.env.AIRTABLE_DEV_API_KEY, // (set via environment variable for this example)
         tables: [
           {
             baseId: `appv8dCu3p2PAaA2F`,
@@ -15,22 +51,23 @@ module.exports = {
             tableView: `All`,
             queryName: `test_default_values`,
             defaultValues: {
-              random_field_doesnt_exist: 'This sentence is only in our config. We do not even have a column in Airtable with this name.',
-              Body: 'We fill in the body from the _default_.',
-              Pages: [`rec4W0c54F3cGvVHh`]
+              random_field_doesnt_exist:
+                "This sentence is only in our config. We do not even have a column in Airtable with this name.",
+              Body: "We fill in the body from the _default_.",
+              Pages: [`rec4W0c54F3cGvVHh`],
             },
-            mapping: { Body: 'text/markdown' },
+            mapping: { Body: "text/markdown" },
             tableLinks: [`Pages`],
           },
           {
             baseId: `appv8dCu3p2PAaA2F`,
             tableName: `Pages`,
             tableView: `All`,
-            mapping: { Body: 'text/markdown' },
+            mapping: { Body: "text/markdown" },
             tableLinks: [`Section`],
           },
         ],
       },
     },
   ],
-}
+};
